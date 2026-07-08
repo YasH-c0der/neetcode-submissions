f58e40b0de03c273a67d032b691e28d5a8d63919
YASH AGARWAL <153943360+YasH-c0der@users.noreply.github.com>
2026-07-08 15:00:43 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map <char, int> mpp;
+        for(int i = 0; i<s.size(); i++){
+            if(mpp.find(s[i]) == mpp.end()){
+                mpp[s[i]] = 1;
+            }
+            else{
+                mpp[s[i]]++;
+            }
+        }
+        for(int i = 0; i<t.size(); i++){
+            if(mpp.find(t[i]) == mpp.end()){
+                return false;
+            }
+            else{
+                mpp[t[i]]--;
+            }
+        }
+        for(auto it:mpp){
+            if(it.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
